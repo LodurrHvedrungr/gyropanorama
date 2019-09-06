fd460d4f356598a333941ab742929813c48f2358
LodurrWasTaken <mrr3alme@gmail.com>
2019-09-06 13:53:27 +0300

    port

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'misc_controls_deviceorientation.html')))
 
-app.listen(8080, console.log('listening 8080...'))
+app.listen(process.env.PORT || 8080, console.log('listening 8080...'))
