c1c5a2f68a181b8d4eb00a5ec4adb8c7eb50ba64
LodurrWasTaken <mrr3alme@gmail.com>
2019-09-27 15:37:39 +0300

    in

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require('path')
 
 app.use(express.static(__dirname))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'misc_controls_deviceorientation.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 app.listen(process.env.PORT || 8080, console.log('listening 8080...'))
